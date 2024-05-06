eeba5ca336de204374acef6c91ceb33e2d160908
fred <fred@gmail.com>
2024-05-06 16:05:17 -0700

    Dialogo entre amigos

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,13 @@
+print("===========  Dialogo entre personas ==========")
+
+print("Ricardo: !Hola Maria¡")
+print("Maria: !Hola Ricardo, tanto tiempo¡. Que casualidad encontrarnos en el mercado, ¿no?")
+print("Ricardo: Si, realmente. ¿Como has estado todos estos años?. Pense que estabas viviendo fuera del pais.")
+print("Maria: Si. Estuve muchos años viviendo en Argentina, pero hace unos meses volvi para quedarme.")
+print("Ricardo: Y. ¿Porque no aprovechamos este encuentro y arreglamos para almorzar algun dia?")
+print("Maria: Me encanta la idea. Para el martes, ¿Que dices?")
+print("Ricardo: Perfecto, justo estoy libre. Te dejo mi numero para que definamos la hora y el lugar, ¿te parece?")
+print("Maria: Me parece genial. !Que alegria volver a verte¡. Nos vemos el martes entonces.")
+print("Ricardo: Lo mismo digo, !Nos vemos pronto¡. !Adios¡")
+
+
