2aa840caca0fa85dc62b0d8a51b22b1ad2332e17
fred <fred@gmail.com>
2024-05-08 06:40:29 -0700

    Ejercicio 1, crea un cuadrado con prints

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,9 @@
+# Crear un archivo "ejercicio_01.py", añade un
+# coentario con la descripcion del ejercicio 1
+# y crea un cuadrado con prints
+
+print("********")
+print("*      *")
+print("*      *")
+print("*      *")
+print("********")
